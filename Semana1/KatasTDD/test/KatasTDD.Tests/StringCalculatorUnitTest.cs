1606ced853de6fa697564c4de0bbe4717e1d3668
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-17 18:08:50 -0500

    🟥 Calculate_IfTheInputIsOnlyOneNumber_ShouldReturnThatNumber

--- Semana1/KatasTDD/test/KatasTDD.Tests/StringCalculatorUnitTest.cs
+++ Semana1/KatasTDD/test/KatasTDD.Tests/StringCalculatorUnitTest.cs
@@ -14,6 +14,16 @@ public class StringCalculatorTest
         result.Should().Be(0);
     }
 
+    [Fact]
+    public void Calculate_IfTheInputIsOnlyOneNumber_ShouldReturnThatNumber()
+    {
+        string input = "1";
+
+        int result = Calculate(input);
+
+        result.Should().Be(1);
+    }
+
     private static int Calculate(string input)
     {
         return 0;
