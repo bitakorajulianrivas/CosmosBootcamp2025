0e2cf28172fd17f6dd31474c316c087258e92e02
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-17 17:58:42 -0500

    🟥 Calculate_IfTheInputIsAnEmptyString_ShouldReturnZero

--- Semana1/KatasTDD/test/KatasTDD.Tests/StringCalculatorUnitTest.cs
+++ Semana1/KatasTDD/test/KatasTDD.Tests/StringCalculatorUnitTest.cs
@@ -1,5 +1,20 @@
-﻿namespace KatasTDD.Tests;
+﻿using FluentAssertions;
+
+namespace KatasTDD.Tests;
 
 public class StringCalculatorTest
 {
+    [Fact]
+    public void Calculate_IfTheInputIsAnEmptyString_ShouldReturnZero()
+    {
+        var input = "";
+        var result = Calculate(input);
+
+        result.Should().Be(0);
+    }
+
+    private int Calculate(string input)
+    {
+        throw new NotImplementedException();
+    }
 }
