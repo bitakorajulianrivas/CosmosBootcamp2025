5e20b5e5dc1d35b413e97a98ffaa78b2e817840c
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-17 23:08:54 -0500

    🟦 Calculate_IfTheInputNumbersAreBiggerThan1000_ShouldIgnorethoseNumbersAndSumRemaining

---
+++ Semana1/KatasTDD/src/KatasTDD.Domain/StringCalculator.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+
+namespace KatasTDD.Domain;
+
+public static class StringCalculator
+{
+    public static int Calculate(string? input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return 0;
+        
+        return SumParsedNumbers(input);
+    }
+
+    private static int SumParsedNumbers(string input)
+    {
+        int[] integers = ExtractIntegers(input);
+
+        VerifyIfExistAnyNegative(integers);
+
+        return integers.Sum();
+    }
+
+    private static int[] ExtractIntegers(string input)
+    {
+        string onlyIntegersPattern = @"[-+]?\d+";
+
+        int[] numbers = Regex.Matches(input, onlyIntegersPattern)
+            .Select(match => int.Parse(match.Value))
+            .Where(number => number <= 1000)
+            .ToArray();
+
+        return numbers;
+    }
+
+    private static void VerifyIfExistAnyNegative(int[] numbers)
+    {
+        int[] negatives = numbers
+            .Where(number => number < 0)
+            .ToArray();
+
+        if(negatives.Length > 0)
+            throw new ArgumentException(
+                $"Negatives are not allowed: {string.Join(' ', negatives)}.");
+    }
+}
--- Semana1/KatasTDD/test/KatasTDD.Tests/StringCalculatorUnitTest.cs
+++ Semana1/KatasTDD/test/KatasTDD.Tests/StringCalculatorUnitTest.cs
@@ -1,5 +1,5 @@
-﻿using System.Text.RegularExpressions;
-using FluentAssertions;
+﻿using FluentAssertions;
+using KatasTDD.Domain;
 
 namespace KatasTDD.Tests;
 
@@ -10,7 +10,7 @@ public class StringCalculatorTest
     {
         string? input = null;
 
-        int result = Calculate(input);
+        int result = StringCalculator.Calculate(input);
 
         result.Should().Be(0);
     }
@@ -20,7 +20,7 @@ public class StringCalculatorTest
     {
         string input = string.Empty;
 
-        int result = Calculate(input);
+        int result = StringCalculator.Calculate(input);
 
         result.Should().Be(0);
     }
@@ -30,7 +30,7 @@ public class StringCalculatorTest
     {
         string input = "1";
 
-        int result = Calculate(input);
+        int result = StringCalculator.Calculate(input);
 
         result.Should().Be(1);
     }
@@ -40,7 +40,7 @@ public class StringCalculatorTest
     {
         string input = "2";
 
-        int result = Calculate(input);
+        int result = StringCalculator.Calculate(input);
 
         result.Should().Be(2);
     }
@@ -50,7 +50,7 @@ public class StringCalculatorTest
     {
         string input = "1,2";
 
-        int result = Calculate(input);
+        int result = StringCalculator.Calculate(input);
 
         result.Should().Be(3);
     }
@@ -60,7 +60,7 @@ public class StringCalculatorTest
     {
         string input = "1,2,3,4,5";
 
-        int result = Calculate(input);
+        int result = StringCalculator.Calculate(input);
 
         result.Should().Be(15);
     }
@@ -70,7 +70,7 @@ public class StringCalculatorTest
     {
         string input = "1\\n2,3";
 
-        int result = Calculate(input);
+        int result = StringCalculator.Calculate(input);
 
         result.Should().Be(6);
     }
@@ -80,7 +80,7 @@ public class StringCalculatorTest
     {
         string input = "1,\\n2,3";
 
-        int result = Calculate(input);
+        int result = StringCalculator.Calculate(input);
 
         result.Should().Be(6);
     }
@@ -90,7 +90,7 @@ public class StringCalculatorTest
     {
         string input = "//;\\n1;2;3";
 
-        int result = Calculate(input);
+        int result = StringCalculator.Calculate(input);
 
         result.Should().Be(6);
     }
@@ -100,7 +100,7 @@ public class StringCalculatorTest
     {
         string input = "1,-2,-3";
 
-        Action action = () => Calculate(input);
+        Action action = () => StringCalculator.Calculate(input);
 
         action.Should().ThrowExactly<ArgumentException>()
             .WithMessage("Negatives are not allowed: -2 -3.");
@@ -111,49 +111,8 @@ public class StringCalculatorTest
     {
         string input = "1001, 2";
 
-        int result = Calculate(input);
+        int result = StringCalculator.Calculate(input);
 
         result.Should().Be(2);
-    }
-
-    private static int Calculate(string? input)
-    {
-        if (string.IsNullOrEmpty(input))
-            return 0;
-        
-        return SumParsedNumbers(input);
-    }
-
-    private static int SumParsedNumbers(string input)
-    {
-        if (input.StartsWith("1001"))
-            input = input.Replace("1001", "0");
-
-        var integers = ExtractIntegers(input);
-
-        VerifyIfExistAnyNegative(integers);
-
-        return integers.Sum();
-    }
-
-    private static int[] ExtractIntegers(string input)
-    {
-        string onlyIntegersPattern = @"[-+]?\d+";
-
-        int[] numbers = Regex.Matches(input, onlyIntegersPattern)
-            .Select(match => int.Parse(match.Value))
-            .ToArray();
-        return numbers;
-    }
-
-    private static void VerifyIfExistAnyNegative(int[] numbers)
-    {
-        int[] negatives = numbers
-            .Where(number => number < 0)
-            .ToArray();
-
-        if(negatives.Length > 0)
-            throw new ArgumentException(
-                $"Negatives are not allowed: {string.Join(' ', negatives)}.");
     }
 }
