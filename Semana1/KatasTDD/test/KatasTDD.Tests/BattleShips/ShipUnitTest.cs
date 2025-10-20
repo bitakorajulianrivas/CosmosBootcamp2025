f78058c75d2f2da5b5c018ba3aab3ea479877130
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-20 17:26:16 -0500

    🟥 Ship_ShouldHaveASize

---
+++ Semana1/KatasTDD/test/KatasTDD.Tests/BattleShips/ShipUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+
+namespace KatasTDD.Tests.BattleShips;
+
+public class ShipUnitTest
+{
+    [Fact]
+    public void Ship_ShouldHaveASize()
+    {
+        var ship = new Ship(ShipType.Destroyer, size: 3);
+
+        ship.Size.Should().Be(3);
+    }
+}
+
+public class Ship
+{
+    public Ship(ShipType type, int size)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object Size { get; set; }
+}
