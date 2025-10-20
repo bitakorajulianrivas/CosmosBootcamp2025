adb03cfe2a1421794bf628b58790666cbb8786f6
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-20 17:21:29 -0500

    Move BoardTest.cs

---
+++ Semana1/KatasTDD/test/KatasTDD.Tests/BattleShips/BoardUnitTest.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+
+namespace KatasTDD.Tests.BattleShips;
+
+public class BoardUnitTest
+{
+    [Fact]
+    public void Board_ShouldHave10Columns()
+    {
+        Board.Columns.Should().Be(10);
+    }
+
+    [Fact]
+    public void Board_ShouldHave10Rows()
+    {
+        Board.Rows.Should().Be(10);
+    }
+
+    [Fact]
+    public void Board_ShouldHave10x10Cells()
+    {
+        var board = new Board();
+
+        board.Cells.GetLength(0).Should().Be(10);
+        board.Cells.GetLength(1).Should().Be(10);
+    }
+
+    [Fact]
+    public void Board_ShouldStartEmpty()
+    {
+        var board = new Board();
+
+        board.AssertAllCellsAreEmpty();
+    }
+}
+
+public class Board
+{
+    public static readonly string EmptyCell = " ";
+
+    public const int Columns = 10;
+    public const int Rows = 10;
+
+    public string[,] Cells { get; }
+
+    public Board()
+    {
+        Cells = new string[Columns, Rows];
+
+        for (int column = 0; column < Columns; column++)
+        for (int row = 0; row < Rows; row++)
+            Cells[column, row] = EmptyCell;
+    }
+}
+
+public static class BoardTestExtensions
+{
+    public static void AssertAllCellsAreEmpty(this Board board)
+    {
+        for (int column = 0; column < Board.Columns; column++)
+        for (int row = 0; row < Board.Columns; row++)
+            board.Cells[column, row].Should().Be(Board.EmptyCell);
+    }
+}
--- Semana1/KatasTDD/test/KatasTDD.Tests/BattleShips/PlayerUnitTest.cs
+++ Semana1/KatasTDD/test/KatasTDD.Tests/BattleShips/PlayerUnitTest.cs
@@ -47,35 +47,6 @@ public class PlayerUnitTest
         player.ShipTypes[ShipType.Destroyer].Should().Be(2);
         player.ShipTypes[ShipType.Gunship].Should().Be(4);
     }
-
-    [Fact]
-    public void Board_ShouldHave10Columns()
-    {
-        Board.Columns.Should().Be(10);
-    }
-
-    [Fact]
-    public void Board_ShouldHave10Rows()
-    {
-        Board.Rows.Should().Be(10);
-    }
-
-    [Fact]
-    public void Board_ShouldHave10x10Cells()
-    {
-        var board = new Board();
-
-        board.Cells.GetLength(0).Should().Be(10);
-        board.Cells.GetLength(1).Should().Be(10);
-    }
-
-    [Fact]
-    public void Board_ShouldStartEmpty()
-    {
-        var board = new Board();
-
-        board.AssertAllCellsAreEmpty();
-    }
 }
 
 public class Player(string nickname)
@@ -95,32 +66,3 @@ public enum ShipType
     Destroyer = 1,
     Gunship = 2
 };
-
-public class Board
-{
-    public static readonly string EmptyCell = " ";
-
-    public const int Columns = 10;
-    public const int Rows = 10;
-
-    public string[,] Cells { get; }
-
-    public Board()
-    {
-        Cells = new string[Columns, Rows];
-
-        for (int column = 0; column < Columns; column++)
-            for (int row = 0; row < Rows; row++)
-                Cells[column, row] = EmptyCell;
-    }
-}
-
-public static class BoardTestExtensions
-{
-    public static void AssertAllCellsAreEmpty(this Board board)
-    {
-        for (int column = 0; column < Board.Columns; column++)
-            for (int row = 0; row < Board.Columns; row++)
-                board.Cells[column, row].Should().Be(Board.EmptyCell);
-    }
-}
