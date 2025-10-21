adce962af7bb591973203a7de5032c825a00ba7f
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-20 20:18:21 -0500

    🟥 Ship_ShouldHavePosition

--- Semana1/KatasTDD/test/KatasTDD.Tests/BattleShips/ShipUnitTest.cs
+++ Semana1/KatasTDD/test/KatasTDD.Tests/BattleShips/ShipUnitTest.cs
@@ -8,15 +8,23 @@ public class ShipUnitTest
     [Fact]
     public void Ship_ShouldHaveCoordinates()
     {
-        var ship = new Ship(ShipType.Carrier, coordinates: (X: 0, Y: 0));
+        var ship = new Ship(ShipType.Carrier, coordinates: (X: 0, Y: 0), position: 0);
 
         ship.Coordinates.Should().Be((0, 0));
+    }
+
+    [Fact]
+    public void Ship_ShouldHavePosition()
+    {
+        var ship = new Ship(ShipType.Carrier, coordinates: (X: 0, Y: 0), position: 0);
+
+        ship.Position.Should().Be(0);
     }
 }
 
 public class Ship
 {
-    public Ship(ShipType shipType, (int X, int Y) coordinates)
+    public Ship(ShipType shipType, (int X, int Y) coordinates, int position)
     {
         ShipType = shipType;
         Coordinates = coordinates;
@@ -24,4 +32,5 @@ public class Ship
 
     public ShipType ShipType { get; }
     public (int X, int Y) Coordinates { get; }
+    public object? Position { get; set; }
 }
