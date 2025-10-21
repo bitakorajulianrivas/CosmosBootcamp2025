6dc4e8839c902eabffc7fdc247bb93e782a102d8
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-21 00:36:02 -0500

    🟩 PlaceShipOnBoard_IfThereAreNoShipsPlaced_ShouldAddShipsPlacedPerType

--- Semana1/KatasTDD/src/KatasTDD.Domain/BattleShips/Player.cs
+++ Semana1/KatasTDD/src/KatasTDD.Domain/BattleShips/Player.cs
@@ -2,25 +2,31 @@
 
 namespace KatasTDD.Domain.BattleShips;
 
-public class Player(string nickname)
+public class Player
 {
-    public string Nickname { get; } = nickname;
-    public Board Board => new ();
+    public string Nickname { get; }
+    public Board Board { get; }
+    public Dictionary<ShipType, int> ShipsPlacedPerType { get; set; }
+
+    public Player(string nickname)
+    {
+        Nickname = nickname;
+        Board = new Board();
+        ShipsPlacedPerType = new Dictionary<ShipType, int>()
+        {
+            {ShipType.Carrier, 0},
+            {ShipType.Destroyer, 0},
+            {ShipType.Gunship, 0}
+        };
+    }
 
     public Dictionary<ShipType, int> ShipTypes { get; } = new () {
         {ShipType.Carrier, 1},
         {ShipType.Destroyer, 2},
         {ShipType.Gunship, 4} };
 
-    public Dictionary<ShipType, int> ShipsPlacedPerType => new ()
-    {
-        {ShipType.Carrier, 0},
-        {ShipType.Destroyer, 0},
-        {ShipType.Gunship, 0}
-    };
-
     public void PlaceShipOnBoard(Ship ship)
     {
-        throw new NotImplementedException();
+        ShipsPlacedPerType[ship.ShipType] = 1;
     }
 }
