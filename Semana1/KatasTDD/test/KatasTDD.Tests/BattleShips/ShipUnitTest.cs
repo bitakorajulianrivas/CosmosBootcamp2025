e08c2bac7f0429f7088a1fb53eb145388c92d5a9
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-20 20:29:24 -0500

    🟦 Ship_ShouldHavePosition_HorizontalAsDefault

--- Semana1/KatasTDD/test/KatasTDD.Tests/BattleShips/ShipUnitTest.cs
+++ Semana1/KatasTDD/test/KatasTDD.Tests/BattleShips/ShipUnitTest.cs
@@ -8,30 +8,36 @@ public class ShipUnitTest
     [Fact]
     public void Ship_ShouldHaveCoordinates()
     {
-        var ship = new Ship(ShipType.Carrier, coordinates: (X: 0, Y: 0), position: 0);
+        var ship = new Ship(ShipType.Carrier, coordinates: (X: 0, Y: 0));
 
         ship.Coordinates.Should().Be((0, 0));
     }
 
     [Fact]
-    public void Ship_ShouldHavePosition()
+    public void Ship_ShouldHavePosition_HorizontalAsDefault()
     {
-        var ship = new Ship(ShipType.Carrier, coordinates: (X: 0, Y: 0), position: 0);
+        var ship = new Ship(ShipType.Carrier, coordinates: (X: 0, Y: 0));
 
-        ship.Position.Should().Be(0);
+        ship.Position.Should().Be(ShipPosition.Horizontal);
     }
+
 }
 
 public class Ship
 {
-    public Ship(ShipType shipType, (int X, int Y) coordinates, int position)
+    public Ship(ShipType shipType, (int X, int Y) coordinates, ShipPosition position = ShipPosition.Horizontal)
     {
         ShipType = shipType;
         Coordinates = coordinates;
-        Position = 0;
+        Position = position;
     }
 
     public ShipType ShipType { get; }
     public (int X, int Y) Coordinates { get; }
-    public object? Position { get; set; }
+    public ShipPosition Position { get; set; }
+}
+
+public enum ShipPosition : byte
+{
+    Horizontal = 0,
 }
