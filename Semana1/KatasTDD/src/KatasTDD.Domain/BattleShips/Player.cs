702dd5262087d2aaaf0bb72f4aa221000ee75469
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-21 00:48:47 -0500

    🟦 PlaceShipOnBoard_IfAlreadyPlacedACarrierOnBoard_ShouldThrowException

--- Semana1/KatasTDD/src/KatasTDD.Domain/BattleShips/Player.cs
+++ Semana1/KatasTDD/src/KatasTDD.Domain/BattleShips/Player.cs
@@ -27,7 +27,8 @@ public class Player
 
     public void PlaceShipOnBoard(Ship ship)
     {
-        if (ship.ShipType == ShipType.Carrier && ShipsPlacedPerType[ShipType.Carrier] >= 1)
+        if (ship.ShipType == ShipType.Carrier && 
+            ShipsPlacedPerType[ship.ShipType] >= 1)
             throw new Exception("All carriers have been placed on the board.");
 
         Board.PlaceShip(ship);
