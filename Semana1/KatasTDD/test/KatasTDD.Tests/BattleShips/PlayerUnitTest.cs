ca9b57fb51bf47939722be42caf650a423dcaa51
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-21 00:55:25 -0500

    🟥 PlaceShipOnBoard_IfAlreadyPlacedTwoDestroyersOnBoard_ShouldThrowException

--- Semana1/KatasTDD/test/KatasTDD.Tests/BattleShips/PlayerUnitTest.cs
+++ Semana1/KatasTDD/test/KatasTDD.Tests/BattleShips/PlayerUnitTest.cs
@@ -127,7 +127,7 @@ public class PlayerUnitTest
     }
 
     [Fact]
-    public void ()
+    public void PlaceShipOnBoard_IfAlreadyPlacedTwoDestroyersOnBoard_ShouldThrowException()
     {
         var player = new Player(nickname: "CaptainAugustus");
 
