e32d448cd18218d6bcbff3ad627559446944aa41
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-27 17:51:43 -0500

    🟥 IsValid_IfTheInputIsNull_ReturnFalse

--- Semana1/KatasTDD/test/KatasTDD.Tests/PasswordValidators/PasswordValidatorUnitTest.cs
+++ Semana1/KatasTDD/test/KatasTDD.Tests/PasswordValidators/PasswordValidatorUnitTest.cs
@@ -1,5 +1,23 @@
-﻿namespace KatasTDD.Tests.PasswordValidators;
+﻿using FluentAssertions;
+
+namespace KatasTDD.Tests.PasswordValidators;
 
 public class PasswordValidatorUnitTest
 {
+    [Fact]
+    public void IsValid_IfTheInputIsNull_ReturnFalse()
+    {
+        string? input = null;
+        bool isValid = PasswordValidator.IsValid(input);
+
+        isValid.Should().BeFalse();
+    }
+}
+
+public static class PasswordValidator
+{
+    public static bool IsValid(string? input)
+    {
+        throw new NotImplementedException();
+    }
 }
