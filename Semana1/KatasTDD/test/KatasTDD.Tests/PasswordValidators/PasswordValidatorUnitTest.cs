ebdf4a02fcb91326a0761111d0e340cd1cf8e21f
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-27 18:43:55 -0500

    🟥 IsValid_IfTheInputDoesNotContainsALowercaseLetter_returnFalse

--- Semana1/KatasTDD/test/KatasTDD.Tests/PasswordValidators/PasswordValidatorUnitTest.cs
+++ Semana1/KatasTDD/test/KatasTDD.Tests/PasswordValidators/PasswordValidatorUnitTest.cs
@@ -27,7 +27,7 @@ public class PasswordValidatorUnitTest
     [Fact]
     public void IsValid_IfTheInputIsLessOrEqualToEightCaracters_ReturnFalse()
     {
-        string input = "password";
+        string input = "Password";
 
         bool isValid = PasswordValidator.IsValid(input);
 
@@ -43,6 +43,16 @@ public class PasswordValidatorUnitTest
 
         isValid.Should().BeFalse();
     }
+
+    [Fact]
+    public void IsValid_IfTheInputDoesNotContainsALowercaseLetter_returnFalse()
+    {
+        string input = "PASSWORDWITHOUTLOWERCASELETTER";
+
+        bool isValid = PasswordValidator.IsValid(input);
+
+        isValid.Should().BeFalse();
+    }
 }
 
 public static class PasswordValidator
@@ -50,11 +60,20 @@ public static class PasswordValidator
     public static bool IsValid(string? input)
     {
         if (string.IsNullOrEmpty(input))
+        {
             return false;
+        }
 
-        if (input.Length > 8)
+        if (input.Length <= 8)
+        {
             return false;
+        }
 
-        return input.Any(char.IsUpper);
+        if (input.Any(char.IsUpper))
+        {
+            return false;
+        }
+
+        throw new NotImplementedException();
     }
 }
