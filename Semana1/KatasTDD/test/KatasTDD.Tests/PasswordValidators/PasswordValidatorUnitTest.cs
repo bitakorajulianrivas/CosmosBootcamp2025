2d3f008f8369f414b7b278553bd281dc52c1685f
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-27 17:54:52 -0500

    🟥 IsValid_IfTheInputIsEmplty_ReturnFalse

--- Semana1/KatasTDD/test/KatasTDD.Tests/PasswordValidators/PasswordValidatorUnitTest.cs
+++ Semana1/KatasTDD/test/KatasTDD.Tests/PasswordValidators/PasswordValidatorUnitTest.cs
@@ -8,10 +8,21 @@ public class PasswordValidatorUnitTest
     public void IsValid_IfTheInputIsNull_ReturnFalse()
     {
         string? input = null;
+
         bool isValid = PasswordValidator.IsValid(input);
 
         isValid.Should().BeFalse();
     }
+
+    [Fact]
+    public void IsValid_IfTheInputIsEmplty_ReturnFalse()
+    {
+        string? input = string.Empty;
+
+        bool isValid = PasswordValidator.IsValid(input);
+        isValid.Should().BeFalse();
+    }
+
 }
 
 public static class PasswordValidator
