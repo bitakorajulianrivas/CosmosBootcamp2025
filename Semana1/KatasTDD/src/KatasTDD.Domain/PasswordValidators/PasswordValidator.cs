c5370bac46003cfb5414d138d22feab86db7fdca
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-27 23:09:55 -0500

    🟥 IsValid_IfTheInputIsNull_ReturnFalse

---
+++ Semana1/KatasTDD/src/KatasTDD.Domain/PasswordValidators/PasswordValidator.cs
@@ -0,0 +1,21 @@
+﻿namespace KatasTDD.Domain.PasswordValidators;
+
+public static class PasswordValidator
+{
+    public static bool IsValid(string input)
+    {
+        return input.IsNotNullAndNotEmpty() &&
+               input.IsMoreThanEightCharacters() &&
+               input.ContainsCapitalLetters() &&
+               input.ContainsLowerCaseLetters()  &&
+               input.ContainsDigits() &&
+               input.ContainsUnderscores();
+    }
+
+    private static bool IsNotNullAndNotEmpty(this string input) => !string.IsNullOrEmpty(input);
+    private static bool IsMoreThanEightCharacters(this string input) => input.Length >= 8;
+    private static bool ContainsCapitalLetters(this string input) => input.Any(char.IsUpper);
+    private static bool ContainsLowerCaseLetters(this string input) => input.Any(char.IsLower);
+    private static bool ContainsDigits(this string input) => input.Any(char.IsDigit);
+    private static bool ContainsUnderscores(this string input) => input.Any(ch => ch == '_');
+}
---
+++ Semana1/KatasTDD/test/KatasTDD.Tests/PasswordValidators/PasswordValitator2UnitTest.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using KatasTDD.Domain.PasswordValidators;
+
+namespace KatasTDD.Tests.PasswordValidators
+{
+    public class PasswordValitator2UnitTest
+    {
+        [Fact]
+        public void IsValid_IfTheInputIsNull_ReturnFalse()
+        {
+            string input = null;
+
+            bool isValid = PasswordValidator2.IsValid(input);
+
+            isValid.Should().BeFalse();
+        }
+    }
+
+    public class PasswordValidator2
+    {
+        public static bool IsValid(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
