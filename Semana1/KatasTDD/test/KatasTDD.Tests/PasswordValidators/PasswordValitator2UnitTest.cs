57923981fc8bdb6c514435706c4b03715d2440c0
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-27 23:35:29 -0500

    🟦 IsValid_IfTheInputDoesNotContainACapitalLetter_ReturnFalse

--- Semana1/KatasTDD/test/KatasTDD.Tests/PasswordValidators/PasswordValitator2UnitTest.cs
+++ Semana1/KatasTDD/test/KatasTDD.Tests/PasswordValidators/PasswordValitator2UnitTest.cs
@@ -66,7 +66,7 @@ namespace KatasTDD.Tests.PasswordValidators
             if (input.Length <= 6)
                 return false;
 
-            if (input == "passwordwithoutcapitalletter")
+            if (input.Any(char.IsUpper) == false)
                 return false;
 
             throw new NotImplementedException();
