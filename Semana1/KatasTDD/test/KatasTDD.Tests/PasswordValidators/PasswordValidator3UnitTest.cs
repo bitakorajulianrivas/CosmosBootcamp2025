af32d4f708e9bdff0d2121a87d7077fc63511300
Julian Rivas <julian.rivas@sinco.com.co>
2025-10-28 08:51:08 -0500

    🟥 IsValid_IfTheInputIsEqualThan16Chars_ShouldReturnFalse

--- Semana1/KatasTDD/test/KatasTDD.Tests/PasswordValidators/PasswordValidator3UnitTest.cs
+++ Semana1/KatasTDD/test/KatasTDD.Tests/PasswordValidators/PasswordValidator3UnitTest.cs
@@ -41,6 +41,16 @@ public class PasswordValidator3UnitTest
 
         isValid.Should().BeFalse();
     }
+
+    [Fact]
+    public void IsValid_IfTheInputIsEqualThan16Chars_ShouldReturnFalse()
+    {
+        string input = "passwdwith16char";
+
+        bool isValid = _passwordValidator3.IsValid(input);
+
+        isValid.Should().BeFalse();
+    }
 }
 
 public class PasswordValidator3
