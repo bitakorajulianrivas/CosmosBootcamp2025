9e7d22477118a423da39451397e820f8b0916ddd
Julian Rivas <julian.rivas@sinco.com.co>
2025-11-03 23:10:38 -0500

    Retirar_SiLaCantidadSonQuinientos_DebeRetornarUnBilleteDeQuinientos

---
+++ Semana3/KatasTDD/tests/CajeroAutomatico.Tests/CajeroTest.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+
+namespace CajeroAutomatico.Tests;
+
+public class CajeroTest
+{
+    [Fact]
+    public void Retirar_SiLaCantidadSonQuinientos_DebeRetornarUnBilleteDeQuinientos()
+    {
+        int montoSolicitado = 500;
+        Cajero cajero = new Cajero();
+        
+        string montoRetirado = cajero.Retirar(montoSolicitado);
+        
+        montoRetirado.Should().Be("1 billete de 500");
+    }
+}
+
+public class Cajero
+{
+    public string Retirar(int montoSolitado)
+    {
+        if(montoSolitado == 500)
+            return "1 billete de 500";
+        
+        throw new NotImplementedException();
+    }
+}
