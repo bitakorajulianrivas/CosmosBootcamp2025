61e0e162eb1a6faf5bd4aeffd9b0daa7bce7b050
Julian Rivas <julian.rivas@sinco.com.co>
2025-11-14 14:56:26 -0500

    ❤️A

--- Semana4/Katas.WordWrap/Katas.WordWrap.Tests/WordWrapTest.cs
+++ Semana4/Katas.WordWrap/Katas.WordWrap.Tests/WordWrapTest.cs
@@ -1,6 +1,19 @@
-﻿namespace WordWrap.Tests;
+﻿using FluentAssertions;
+
+namespace WordWrap.Tests;
 
 public class WordWrapTest
 {
+    [Fact]
+    public void a()
+    {
+        var result = Wrap("", 1);
+
+        result.Should().Be("");
+    }
     
+    private static string Wrap(string text, int col)
+    {
+        throw new Exception();
+    }
 }
