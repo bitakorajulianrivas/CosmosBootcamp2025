59802643fa23f1c0d178ab2649c760f4643b33cc
Julian Rivas <julian.rivas@sinco.com.co>
2025-11-18 10:29:19 -0500

    🟥  SI_AgregoJugador_Debe_ExistirJugador

---
+++ Semana5/Katas.BattleShip/Katas.Battleship.Tests/BatallaNavalTest.cs
@@ -0,0 +1,39 @@
+﻿using AwesomeAssertions;
+
+namespace Katas.Battleship.Tests;
+
+public class BatallaNavalTest
+{
+    [Fact]
+    public void SI_AgregoJugador_Debe_ExistirJugador()
+    {
+        var apodo = "pollo";
+        var jugador = new Jugador(apodo);
+
+        var batallaNaval = new BatallaNaval();
+        batallaNaval.AgregarJugador(jugador);
+        
+        batallaNaval.Jugador1.Apodo.Should().Be("pollo");
+
+    }
+}
+
+public class BatallaNaval
+{
+    public void AgregarJugador(Jugador jugador)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Jugador Jugador1 { get; set; }
+}
+
+public class Jugador
+{
+    public Jugador(string apodo)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Apodo { get; set; }
+}
