7a564ff468958b33c007e62333b518f97923abaa
Julian Rivas <julian.rivas@sinco.com.co>
2025-11-18 16:49:17 -0500

    🟩SI_Tengo1CarrierYAgregoOtro_Debe_LanzarExcepcion

--- Semana5/Katas.BattleShip/Katas.Battleship.Tests/Jugador.cs
+++ Semana5/Katas.BattleShip/Katas.Battleship.Tests/Jugador.cs
@@ -5,11 +5,13 @@ public class Jugador
     private const string YaExisteBarcoEnLaPosiciónEnviada = "Ya existe barco en la posición enviada.";
     public string Apodo { get; private set; }
     public char[,] Tablero { get; set; }
+    public int CantidadCarrier { get; set; }
 
     public Jugador(string apodo)
     {
         Apodo = apodo;
         Tablero = new char[10, 10];
+        CantidadCarrier = 0;
     }
 
 
@@ -17,7 +19,8 @@ public class Jugador
     {
         if (Tablero[x, y] != '\0')
             throw new ArgumentException(YaExisteBarcoEnLaPosiciónEnviada);
-        
+        if (CantidadCarrier >= barco.CantidadBarcos)
+            throw new ArgumentException("Solo se puede Asignar un Carrier");
         AsignarBarco(barco, x, y, esVertical);
     }
 
@@ -30,5 +33,7 @@ public class Jugador
             else
                 Tablero[x + indice, y] = barco.Letra;
         }
+
+        CantidadCarrier++;
     }
 }
