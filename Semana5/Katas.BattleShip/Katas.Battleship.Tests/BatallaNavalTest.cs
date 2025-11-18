fc0d06df3c3934b9f43047595b52da9c5ab9db37
Julian Rivas <julian.rivas@sinco.com.co>
2025-11-18 10:38:09 -0500

    🟥 Si_AgregoJugador2_Debe_ExistirJugador2

--- Semana5/Katas.BattleShip/Katas.Battleship.Tests/BatallaNavalTest.cs
+++ Semana5/Katas.BattleShip/Katas.Battleship.Tests/BatallaNavalTest.cs
@@ -14,12 +14,24 @@ public class BatallaNavalTest
         
         batallaNaval.Jugador1.Apodo.Should().Be("pollo");
     }
+    
+    [Fact]
+    public void Si_AgregoJugador2_Debe_ExistirJugador2()
+    {
+        var jugador2 = new Jugador("gato");
+        var batallaNaval = new BatallaNaval();
+            
+        batallaNaval.AgregarJugador(jugador2);
+        
+        batallaNaval.Jugador2.Apodo.Should().Be("gato");
+    }
 }
 
 public class BatallaNaval
 {
     public Jugador Jugador1 { get; private set; }
-    
+    public Jugador Jugador2 { get; private set; }
+
     public void AgregarJugador(Jugador jugador)
     {
         Jugador1 = jugador;
