af7ab19d5d6e436eb22661db93749f5fcb84da8c
Julian Rivas <julian.rivas@sinco.com.co>
2025-11-19 16:06:27 -0500

    🟥 Si_ObtieneCoordenadas_Debe_RetornarCantidadDePosicionesVerticales

--- Semana5/Katas.BattleShip/Katas.Battleship.Tests/BarcoTest.cs
+++ Semana5/Katas.BattleShip/Katas.Battleship.Tests/BarcoTest.cs
@@ -12,4 +12,14 @@ public class BarcoTest
         barco.ObtenerCoordenadas().Should().BeEquivalentTo([
             (1,1), (2,1), (3,1), (4,1)]);
     }
+    
+    [Fact]
+    public void Si_ObtieneCoordenadas_Debe_RetornarCantidadDePosicionesVerticales()
+    {
+        var barco = Barco.Carrier(Posicion.Vertical(1, 1));
+        
+        barco.ObtenerCoordenadas().Should().BeEquivalentTo([
+            (1,1), (1, 2), (1, 3), (1, 4)]);
+    }
+    
 }
