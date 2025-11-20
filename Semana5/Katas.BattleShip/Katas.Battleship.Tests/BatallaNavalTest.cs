73a76051d5fe177e0ca01a811ced66e642dfbf3d
Julian Rivas <julian.rivas@sinco.com.co>
2025-11-20 08:53:30 -0500

    🟥 Si_RealizoUnDisparoAcertado_Debe_RetornarMensajeTiroAcertado

--- Semana5/Katas.BattleShip/Katas.Battleship.Tests/BatallaNavalTest.cs
+++ Semana5/Katas.BattleShip/Katas.Battleship.Tests/BatallaNavalTest.cs
@@ -816,4 +816,37 @@ public class BatallaNavalTest
 
         mensaje.Should().Be("Disparo fallido en la posicion (0, 2)");
     }
+    [Fact] 
+    public void Si_RealizoUnDisparoAcertado_Debe_RetornarMensajeTiroAcertado()
+    {
+        var batallaNaval = new BatallaNavalBuilder()
+            .AgregarJugador("Pollo")
+            .AgregarJugador("Gato")
+            .ValidarJugadores()
+            .AgregarBarcosJugador1([
+                Barco.Carrier(Posicion.Horizontal(1,1)),   
+                Barco.Destroyer(Posicion.Horizontal(2,2)), 
+                Barco.Destroyer(Posicion.Horizontal(3,3)), 
+                Barco.Gunship(Posicion.Horizontal(4,4)),   
+                Barco.Gunship(Posicion.Horizontal(5, 5)),  
+                Barco.Gunship(Posicion.Horizontal(6,6)),   
+                Barco.Gunship(Posicion.Horizontal(7,7)),   
+            ])
+            .AgregarBarcosJugador2([
+                Barco.Carrier(Posicion.Vertical(1,4)),   
+                Barco.Destroyer(Posicion.Horizontal(1,0)), 
+                Barco.Destroyer(Posicion.Vertical(8,1)), 
+                Barco.Gunship(Posicion.Horizontal(0,2)),   
+                Barco.Gunship(Posicion.Horizontal(0,9)),   
+                Barco.Gunship(Posicion.Horizontal(3,4)),   
+                Barco.Gunship(Posicion.Horizontal(6,7)),   
+            ])
+            .Construir();
+        
+        batallaNaval.Iniciar();
+        //Jugador 1
+        string mensaje = batallaNaval.Disparar(1, 4);
+
+        mensaje.Should().Be("Disparo acertado en la posicion (1, 4)");
+    }
 }
